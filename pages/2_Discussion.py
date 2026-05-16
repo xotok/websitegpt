e4798f852cb5a968ebf1e7850898888ef1e459c8
xd684 <dxkoto@yahoo.com>
2026-05-16 18:33:59 +0200

    Update discussion

--- pages/2_Discussion.py
+++ pages/2_Discussion.py
@@ -1,15 +1,8 @@
 import streamlit as st
 from langchain_core.messages import HumanMessage, AIMessage
 import time
-from utils import (
-    set_openai_api_key,
-    init_session_state,
-    load_database,
-    create_chain,
-    reset_conversation,
-    get_conversation_stats,
-    load_pinecone_database
-    )
+from utils import set_openai_api_key, init_session_state, load_database, create_chain, reset_conversation, get_conversation_stats, load_pinecone_database
+
 
 st.set_page_config(
     page_title="Chat IA",
